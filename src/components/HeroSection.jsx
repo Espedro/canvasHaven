c1c307103d351c4c9058b5aa2aa8aaf2a1cdfba7
Espedro Neptune <41193718+Espedro@users.noreply.github.com>
2024-06-28 13:27:08 -0400

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,25 +4,12 @@ function HeroSection() {
   return (
     <div className="row">
       <section id="home" className="hero-block">
-        <Carousel fade>
-          <Carousel.Item>
-            <img
-              className="d-block w-100 slide"
-              src="https://images.fineartamerica.com/images-medium-large-5/best-art-choice-award-original-abstract-oil-painting-modern-trees-contemporary-home-deco-gallery-emma-lambert.jpg"
-              alt="First Slide"
-            />
-
-            <Carousel.Caption>
-              <h2>Banff National Park</h2>
-
-              <p>Canada / Banff</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-         
-
-          
-        </Carousel>
+        <div className="container">
+          <h1>Canvas Haven </h1>
+          <h4>
+          Where Art Finds Its Sanctuary
+          </h4>
+        </div>
       </section>
     </div>
   )
