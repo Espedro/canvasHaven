89c0344fe8aca7cd11aa6fc5922e9e7a6c07a991
Espedro Neptune <41193718+Espedro@users.noreply.github.com>
2024-06-28 13:51:38 -0400

    Update PaintingPage.jsx

--- src/pages/PaintingPage.jsx
+++ src/pages/PaintingPage.jsx
@@ -3,11 +3,25 @@ import React from 'react'
 
 function PaintingPage() {
   return (
-    <div>PaintingPage
+   
+
+<div>
+
+<section id="home" className="hero-block">
+        <div className="container">
+          <h1>Our Sanctuary of Paintings </h1>
+         
+        </div>
+      </section>
 
 
+      <div className='container'>
+<br />
 <PaintingList />
     </div>
+      </div>
+
+
   )
 }
 
