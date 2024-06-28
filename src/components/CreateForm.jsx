526db198acdecc00c0f2decd5ee5fba1128ee35b
Espedro Neptune <41193718+Espedro@users.noreply.github.com>
2024-06-28 13:27:04 -0400

    Update CreateForm.jsx

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -15,56 +15,71 @@ function CreateForm() {
   }
 
   return (
+    <div className="container">
     <form
       onSubmit={(e) => {
         e.preventDefault()
         createPainting(paintingInfo)
       }}
     >
+      <div class="mb-3">
       <label htmlFor="name">Name</label>
       <input
         type="text"
         name="name"
         onChange={handleChange}
         value={paintingInfo.name}
+        className="form-control"
       />
+      </div>
 
+      <div class="mb-3">
       <label htmlFor="artist">Artist</label>
       <input
         type="text"
         name="artist"
         onChange={handleChange}
         value={paintingInfo.artist}
+        className="form-control"
       />
-
+</div>
+<div class="mb-3">
       <label htmlFor="genre">Genre</label>
       <input
         type="text"
         name="genre"
         onChange={handleChange}
         value={paintingInfo.genre}
+        className="form-control"
       />
-
+      </div>
+      <div class="mb-3">
       <label htmlFor="description"> Description </label>
       <input
         type="text"
         name="description"
         onChange={handleChange}
         value={paintingInfo.description}
+        className="form-control"
       />
-
+      </div>
+      <div class="mb-3">
       <label htmlFor="image">Image</label>
       <input
         type="text"
         name="image"
         onChange={handleChange}
         value={paintingInfo.image}
+        className="form-control"
       />
-
-      <button type="submit" className="btn">
+</div>
+<center>
+      <button type="submit" className="btn btn-primary w-50">
         create
       </button>
+      </center>
     </form>
+    </div>
   )
 }
 
