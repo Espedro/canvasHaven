fe420277ab8f9b22e77971227a2700627bcb2806
Espedro Neptune <41193718+Espedro@users.noreply.github.com>
2024-06-28 13:27:06 -0400

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -25,61 +25,56 @@ function EditForm({ toggleEdit, setToggleEdit }) {
         }}
       >
         <div class="mb-3">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={currPainting.name}
-          className="form-control"
-        />
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={currPainting.name}
+            className="form-control"
+          />
         </div>
         <div class="mb-3">
+          <label htmlFor="artist">Artist</label>
+          <input
+            type="text"
+            name="artist"
+            onChange={handleChange}
+            value={currPainting.artist}
+            className="form-control"
+          />
+        </div>
+        <div class="mb-3">
+          <label htmlFor="genre">Genre</label>
+          <input
+            type="text"
+            name="genre"
+            onChange={handleChange}
+            value={currPainting.genre}
+            className="form-control"
+          />
+        </div>
+        <div class="mb-3">
+          <label htmlFor="description"> Description </label>
+          <input
+            type="text"
+            name="description"
+            onChange={handleChange}
+            value={currPainting.description}
+            className="form-control"
+          />
+        </div>
 
-        <label htmlFor="artist">Artist</label>
-        <input
-          type="text"
-          name="artist"
-          onChange={handleChange}
-          value={currPainting.artist}
-          className="form-control"
-        />
-
-</div>
-<div class="mb-3">
-        <label htmlFor="genre">Genre</label>
-        <input
-          type="text"
-          name="genre"
-          onChange={handleChange}
-          value={currPainting.genre}
-          className="form-control"
-        />
-
-</div>
-<div class="mb-3">
-        <label htmlFor="description"> Description </label>
-        <input
-          type="text"
-          name="description"
-          onChange={handleChange}
-          value={currPainting.description}
-          className="form-control"
-        />
-
-</div>
-
-<div class="mb-3">
-        <label htmlFor="image">Image</label>
-        <input
-          type="text"
-          name="image"
-          onChange={handleChange}
-          value={currPainting.image}
-          className="form-control"
-        />
-
-</div>
+        <div class="mb-3">
+          <label htmlFor="image">Image</label>
+          <input
+            type="text"
+            name="image"
+            onChange={handleChange}
+            value={currPainting.image}
+            className="form-control"
+          />
+        </div>
 
         <button type="submit" className="btn btn-success w-50">
           Update
