01825d71453fcd80c6dfa478d315c2e0ec1dab3f
Espedro Neptune <41193718+Espedro@users.noreply.github.com>
2024-06-28 13:51:28 -0400

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react"
 import { AuthContext } from "../context/auth.context"
-import { Form, Button, Alert } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap"
 
 function LoginForm() {
   const [loginInfo, setLoginInfo] = useState({
@@ -22,34 +22,30 @@ function LoginForm() {
         login(loginInfo)
       }}
     >
+      <Form.Group className="mb-2" controlId="username">
+        <Form.Label> Username / Email </Form.Label>
+        <Form.Control
+          type="text"
+          name="loginInfo"
+          value={loginInfo.loginInfo}
+          onChange={handleChange}
+        />
+      </Form.Group>
 
+      <Form.Group className="mb-2" controlId="paswword">
+        <Form.Label> Password </Form.Label>
 
-<Form.Group className="mb-2" controlId="username">
-    <Form.Label> Username / Email </Form.Label> 
-      < Form.Control
-        type="text"
-        name="loginInfo"
-        value={loginInfo.loginInfo}
-        onChange={handleChange}
-      />
-  </Form.Group>
-
-  <Form.Group className="mb-2" controlId="paswword">
-  <Form.Label> Password </Form.Label>
-    
-  < Form.Control
-        type="password"
-        name="password"
-        value={loginInfo.password}
-        onChange={handleChange}
-      />
-
-</Form.Group>
-<center>
-      <button type="submit" className="w-50">
-        log in
-      </button>
-
+        <Form.Control
+          type="password"
+          name="password"
+          value={loginInfo.password}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <center>
+        <button type="submit" className="btn btn-primary w-50">
+          log in
+        </button>
       </center>
     </form>
   )
